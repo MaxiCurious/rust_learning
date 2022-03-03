0f955996072ed77ef7ddef406fb29de1e451d148
Guillois, Maxime /FR <maxime.guillois2@sanofi.com>
2022-03-03 16:50:18 +0000

    Add function to get table records

--- web_scrap_cli/src/db_utils.rs
+++ web_scrap_cli/src/db_utils.rs
@@ -40,18 +40,60 @@ pub fn save_selector_records_to_db(conn: &mut Connection, table_name: &str, reco
     return Ok(());
 }
 
+pub fn get_selector_records_from_table(conn: &Connection, table: &str)-> Result<Vec<SelectorRecord>, Box<dyn Error>>{
+    let sql_request = format!("SELECT (timestamp, url, selector, content) FROM {}", table);
+    let mut stmt = conn.prepare(&sql_request)?;
+    let record_iter = stmt.query_map([], |row| {
+        Ok(SelectorRecord {
+            timestamp: row.get(0)?,
+            url: row.get(1)?,
+            selector: row.get(2)?,
+            content: row.get(3)?
+        })
+    })?;
+
+    let mut records = Vec::new();
+    for record in record_iter {
+        println!("1 SelectorRecord loaded");
+       records.push(record?);
+    }
+    return Ok(records);
+}
+
+pub fn get_row_count(conn: &Connection, table: &str) -> Result<u32, Box<dyn Error>> {
+    let sql_request = format!("SELECT COUNT(*) FROM {}", table);
+
+    let mut stmt = conn.prepare(&sql_request)?;
+    let rows = stmt.query_map([], |row| row.get(0))?;
+
+    let mut values = Vec::new();
+    for val in rows {
+       values.push(val?);
+    }
+    
+    println!("Got values : {:?}", values);
+
+    return Ok(values[0]);
+}
 
 #[cfg(test)]
-mod tests {    
+mod tests {        
+    use std::path::Path; 
+
     use super::super::*;  // retrieve all from main
-    use std::path::Path;    
+    use super::{get_row_count, get_selector_records_from_table };
 
     #[test]
     fn test_save_selector_records_to_db() {
         let mut conn = Connection::open(Path::new(&"web_scrap_cli_test.db")).unwrap();
         let table = &"selector_record_test";
-        let mut records: Vec<SelectorRecord> = Vec::new();
 
+        match conn.execute(format!("DROP TABLE IF EXISTS {}", table).as_str(), [],) {
+            Ok(_updated) => println!("'{}' has been dropped", table),
+            Err(err) => panic!("DROP TABLE failed: {}", err),
+        }
+        
+        let mut records: Vec<SelectorRecord> = Vec::new();
         records.push(SelectorRecord{timestamp: get_timestamp_now(),
                                     url: String::from(r"http:\\www.test.fr"),
                                     selector: String::from("a"),
@@ -64,6 +106,11 @@ mod tests {
         );
 
         save_selector_records_to_db(&mut conn, table, &records).unwrap();
+        let row_count = get_row_count(&conn, table).unwrap();        
+        assert_eq!(row_count, 2, "{:?}", row_count);
+
+        let _rows = get_selector_records_from_table(&conn, table).unwrap();
+
         match conn.execute(format!("DROP TABLE {}", table).as_str(), [],) {
             Ok(updated) => println!("{} row inserted in '{}'", updated, table),
             Err(err) => panic!("DROP TABLE failed: {}", err),
